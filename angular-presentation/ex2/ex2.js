2422ec36953574c4d14f7a9a55873a0c12fb92c6
Dileep Cn <dileep.cn@valorem.com>
2018-04-27 12:18:18 +0530

    presentation deleted

--- angular-presentation/ex2/ex2.js
+++
@@ -1,7 +0,0 @@
-function MyCtrl( $scope, $http ) {
-     $scope.FetchJoke = function() {
-        $http.get( "http://api.icndb.com/jokes/random").then( function(response) {
-            $scope.data = response.data;
-        });
-    }
-}
